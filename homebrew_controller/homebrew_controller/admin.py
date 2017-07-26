e764005c57468a88f46a7d0231d0b2be5634099a
smckinless <smckinless@gmail.com>
2017-07-26 10:01:44 -0400

    Add temp reading class to admin panel

--- homebrew_controller/homebrew_controller/admin.py
+++ homebrew_controller/homebrew_controller/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from models import BrewStep, Brew, Probe
+from models import BrewStep, Brew, Probe, TempReading
 
 admin.site.register(BrewStep)
 admin.site.register(Brew)
 admin.site.register(Probe)
+admin.site.register(TempReading)
