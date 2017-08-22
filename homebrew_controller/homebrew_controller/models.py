26ec23887cac31e52177d53394e71a8fd2309ff4
smckinless <smckinless@gmail.com>
2017-08-22 19:13:21 -0400

    Add in text fields for recipe, flavor profile, additional notes

--- homebrew_controller/homebrew_controller/models.py
+++ homebrew_controller/homebrew_controller/models.py
@@ -1,16 +1,23 @@
 from django.db import models
 
+
 class BrewStep(models.Model):
     name = models.CharField(max_length=256)
+
 
 class Brew(models.Model):
     name = models.CharField(max_length=256)
     current_brew_step = models.ForeignKey(BrewStep, null=True)
     is_active = models.BooleanField(default=False)
+    recipe_list = models.TextField(null=True)
+    flavor_profile = models.TextField(null=True)
+    other_notes = models.TextField(null=True)
+
 
 class Probe(models.Model):
     id = models.CharField(max_length=256, primary_key=True)
     location = models.CharField(max_length=256)
+
 
 class TempReading(models.Model):
     brew = models.ForeignKey(Brew)
